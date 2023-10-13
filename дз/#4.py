c2bde23afc0804c1a31a65e20b45a6501229d6d7
IlyaLyamin <liamin.6@yandex.ru>
2023-10-13 08:08:41 +0300

    1 неделя 1,2,3,4,5

--- дз/#4.py
+++ дз/#4.py
@@ -1,11 +1,6 @@
-
-# set_1 = set(map(int, input("Введите первое множество:").split()))
-# set_2 = set(map(int, input("Введите второе множество:").split()))
-# print(set_1 & set_2)
-
+# программа вычисляет пересение двух множеств храня данные в формату int
 def con(A_s = str, B_s = str):
     c = 0
-    print(A_s, B_s)
     for i in range(len(min([A_s, B_s], key=len))):
         c += 1
         if A_s[-(i + 1)] == B_s[-(i + 1)]:
@@ -13,11 +8,7 @@ def con(A_s = str, B_s = str):
 
 
 if __name__ == "__main__":
-    a = set(map(int, input("Введите первое множество:").split()))
-    b = set(map(int, input("Введите второе множество:").split()))
-    A = str(sum([10 ** (i - 1) for i in a]))
-    B = str(sum([10 ** (i - 1) for i in b]))
+    a = set(map(int, input("Введите первое множество: ").split()))
+    b = set(map(int, input("Введите второе множество: ").split()))
+    A, B = str(sum([10 ** (i - 1) for i in a])), str(sum([10 ** (i - 1) for i in b]))
     con(A, B)
-
-    # 1111111
-    # 00011111
--- дз/#5.py
+++ дз/#5.py
@@ -1,3 +1,18 @@
-from math import factorial
+def factorial_(num = int): # вычислет факториал
+    ans = 1
+    for i in range(1, num + 1):
+        ans *= i
+    return ans
 
-print(factorial(int(input())))
+def factorial_zeros(num = int): # вычисляет кол-во нулей факториала
+    c = 1
+    s = 0
+    while (5 ** c) <= num: # находим кол-во делений на 5, 25, 125, 625...
+        s += num // (5 ** c)
+        c += 1
+    return s
+
+if __name__ == "__main__":
+    num = int(input())
+    print(factorial_(num))
+    print(factorial_zeros(num))
